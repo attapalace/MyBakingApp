c08cc64fd15f5599f919e9344b31b83012423fcd
attapalace <attapalace@hotmail.com>
2018-04-16 15:07:33 +0200

    fixed rotation issues and image thumb implementation

--- app/src/main/java/com/examples/apps/atta/mybakingapp/StepDetailFragment.java
+++ app/src/main/java/com/examples/apps/atta/mybakingapp/StepDetailFragment.java
@@ -5,16 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.examples.apps.atta.mybakingapp.model.Recipe;
 import com.examples.apps.atta.mybakingapp.model.Step;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
@@ -27,17 +30,18 @@ import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.examples.apps.atta.mybakingapp.MainActivity.RECIPES_BUNDLE;
 import static com.examples.apps.atta.mybakingapp.RecipeDetailsActivity.STEPS_BUNDLE;
 import static com.examples.apps.atta.mybakingapp.RecipeDetailsActivity.STEP_INDEX;
-import static com.examples.apps.atta.mybakingapp.MainActivity.RECIPES_BUNDLE;
 
-public class StepDetailFragment extends Fragment {
+public class StepDetailFragment extends Fragment  {
 
     ArrayList<Step> steps;
     @BindView(R.id.step_description_tv)
@@ -50,10 +54,18 @@ public class StepDetailFragment extends Fragment {
     SimpleExoPlayerView mPlayerView;
     @BindView(R.id.noVideo_tv)
     TextView emptyVideotv;
+    @BindView(R.id.step_image_thumbnail)
+    ImageView step_thumbnail;
 
     private SimpleExoPlayer mExoPlayer;
     private int stepIndex;
     private StepClickListener stepClickListener;
+    private PlaybackStateCompat.Builder mStateBuilder;
+    long position;
+    private static final String SELECTED_POSITION = "position";
+    private static final String PLAY_WHEN_READY = "play when ready";
+    String videoUrl;
+    boolean playWhenReady;
 
 
     public StepDetailFragment() {
@@ -79,6 +91,8 @@ public class StepDetailFragment extends Fragment {
         if (savedInstanceState != null){
             steps = (ArrayList<Step>) savedInstanceState.getSerializable(STEPS_BUNDLE);
             stepIndex = savedInstanceState.getInt(STEP_INDEX);
+            position = savedInstanceState.getLong(SELECTED_POSITION, C.TIME_UNSET);
+            playWhenReady = savedInstanceState.getBoolean(PLAY_WHEN_READY);
         }else {
             steps = (ArrayList<Step>) getArguments().getSerializable(STEPS_BUNDLE);
             if (steps!=null){
@@ -96,15 +110,17 @@ public class StepDetailFragment extends Fragment {
         stepDescriptiontv.setText(steps.get(stepIndex).getDescription());
         String imageUrl = steps.get(stepIndex).getThumbnailURL();
 
-
-        String videoUrl = steps.get(stepIndex).getVideoURL();
+        videoUrl = steps.get(stepIndex).getVideoURL();
 
         if (videoUrl != ""){
             initializePlayer(Uri.parse(videoUrl));
+            step_thumbnail.setVisibility(View.GONE);
         }else if (imageUrl!=""){
-            initializePlayer(Uri.parse(imageUrl));
+            mPlayerView.setVisibility(View.GONE);
+            Picasso.get().load(imageUrl).into(step_thumbnail);
         }else {
             mPlayerView.setVisibility(View.GONE);
+            step_thumbnail.setVisibility(View.GONE);
             emptyVideotv.setVisibility(View.VISIBLE);
         }
 
@@ -150,9 +166,31 @@ public class StepDetailFragment extends Fragment {
             String userAgent = Util.getUserAgent(getActivity(), "My Baking app");
             MediaSource mediaSource = new ExtractorMediaSource(videoUri, new DefaultDataSourceFactory(
                     getActivity(), userAgent), new DefaultExtractorsFactory(), null, null);
-            mExoPlayer.prepare(mediaSource);
-            mExoPlayer.setPlayWhenReady(true);
 
+            if (position != C.TIME_UNSET) mExoPlayer.seekTo(position);
+
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(playWhenReady);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (videoUrl != ""){
+            initializePlayer(Uri.parse(videoUrl));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mExoPlayer != null){
+            position = mExoPlayer.getCurrentPosition();
+            playWhenReady = mExoPlayer.getPlayWhenReady();
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
         }
     }
 
@@ -175,10 +213,14 @@ public class StepDetailFragment extends Fragment {
         }
     }
 
+
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putSerializable(STEPS_BUNDLE,steps);
         outState.putInt(STEP_INDEX , stepIndex);
+        outState.putLong(SELECTED_POSITION, position);
+        outState.putBoolean(PLAY_WHEN_READY , playWhenReady);
     }
+
 }
